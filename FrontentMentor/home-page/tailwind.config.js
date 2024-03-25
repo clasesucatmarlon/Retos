43fc16d7b4f3291d32c5bfb7de1b8c912e9c5a31
clasesucatmarlon <clasesucatmarlon@gmail.com>
2024-03-25 11:12:07 -0500

    feat(Retos-HomePage): :sparkles: Configurando colores del la app
    Configurando colores del la app

--- FrontentMentor/home-page/tailwind.config.js
+++ FrontentMentor/home-page/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "SoftOrange": "hsl(35, 77%, 62%)",
+        "SoftRed": "hsl(5, 85%, 63%)",
+        "OffWwhite": "hsl(36, 100%, 99%)",
+        "GrayishBblue": "hsl(233, 8%, 79%)",
+        "DarkGrayishBlue": "hsl(236, 13%, 42%)"
+      }
+    },
   },
   plugins: [],
 }
-
